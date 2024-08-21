47d287752191e27c3207e9afae267c5a639b0844
harshal <proskylord69@gmail.com>
2024-08-21 09:14:41 +0530

    nothing

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
- require('dotenv').config();
- 
- 
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,8 +15,8 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Define a schema for the location
 const locationSchema = new mongoose.Schema({
@@ -31,11 +29,10 @@ const locationSchema = new mongoose.Schema({
 });
 
 const Location = mongoose.model('Location', locationSchema);
-module.exports = Location;
 
 // API endpoint to update location
 app.post('/api/location', async (req, res) => {
-  const { busNumber, latitude, longitude,speed, heading,timestamp } = req.body;
+  const { busNumber, latitude, longitude, speed, heading, timestamp } = req.body;
 
   try {
     const updatedLocation = await Location.findOneAndUpdate(
