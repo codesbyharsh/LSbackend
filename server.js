1c10900f0a237dc9a0b8a1273f57236de0beb968
proskylord69 <138246713+proskylord69@users.noreply.github.com>
2025-04-24 20:21:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app   = express();
+app.set('trust proxy', 1);
 const PORT  = process.env.PORT || 5000;
 const SPEED_THRESHOLD = 1;   // m/s
 
